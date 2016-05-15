c542a6ff48356680194c98c68dd699b0e1790cc7
qzer9816 <qzer9816@gmail.com>
2016-05-15 16:45:48 +0800

    加註解

--- BinarySort.java
+++ BinarySort.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
  
 public class BinarySort 
 {
+	//main method
 	public static void main(String args[])
 	{	
+		//data field
 		int first, last, middle;
 		int number;
 		int element[];
 		int search;
 		
+		//new scanner
 		Scanner input = new Scanner(System.in);
 		System.out.println("How many elements you need:");
 		number = input.nextInt();
@@ -18,17 +21,20 @@ public class BinarySort
 	
 		System.out.println("Please enter " + number + " integers:");
 
+		//let number in element array
 		for (int i = 0; i < number; i++){
 			element[i] = input.nextInt();
 		}
 		
+		//let user input search number
 		System.out.println("Enter value to find:");
 		search = input.nextInt();
 		
 		first = 0;
 		last = number - 1;
 		middle = (first + last)/2;
-
+		
+		//binary search
 		while( first <= last ){
 			if ( element[middle] < search ){
 				first = middle + 1;    
